8acf8fbd32538efda289afe67471da021375f0d8
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-11-02 20:29:04 +0300

    Refactor PassangerPlane

--- Planes/PassengerPlane.js
+++ Planes/PassengerPlane.js
@@ -2,21 +2,15 @@ const Plane = require('./Plane');
 
 class PassengerPlane extends Plane {
 
-    //=================CONSTRUCTORS=================
-
-    constructor(model, maxSpeed, maxFlightDistance, maxLoadCapacity, passengersCapacity) {
+    constructor(flyInformation) {
+        const {model, maxSpeed, maxFlightDistance, maxLoadCapacity, passengersCapacity} = {flyInformation}
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
         this._passengersCapacity = passengersCapacity;
     }
 
-    //=================METHODS=================
     getPassengersCapacity() {
         return this._passengersCapacity;
     }
-
-    // set passengersCapacity(value) {
-    //     this._passengersCapacity = value;
-    // }
 }
 
 module.exports = PassengerPlane;
---
+++ test.js
@@ -0,0 +1,6 @@
+const arr ={
+    one:2,
+    two:3
+}
+const {one,two} = {...arr}
+console.log(one,two);
