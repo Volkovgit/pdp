11b48a6148570ec5127f8b066f775daca72b5e88
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-11-07 19:38:33 +0300

    Удалил models

--- models/ExperimentalTypes.js
+++
@@ -1,8 +0,0 @@
-const experimentalTypes   = {
-    LIFTING_BODY: 'LIFTING_BODY',
-    HYPERSONIC: 'HYPERSONIC',
-    HIGH_ALTITUDE: 'HIGH_ALTITUDE',
-    VTOL: "VTOL"
-};
-
-module.exports =  experimentalTypes ;
--- models/classificationLevel.js
+++
@@ -1,8 +0,0 @@
-const classificationLevel  = {
-    UNCLASSIFIED: 'UNCLASSIFIED',
-    CONFIDENTIAL: 'CONFIDENTIAL',
-    SECRET: 'SECRET',
-    SECRETTOP_SECRET: 'SECRETTOP_SECRET'
-};
-
-module.exports =  classificationLevel;
