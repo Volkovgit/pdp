f5cc7155212f35819bcfec576a49c16dceefb16a
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-11-07 17:54:12 +0300

    Убрал все лишнее из ExperimentalPlane

--- Planes/experimentalPlane.js
+++ Planes/experimentalPlane.js
@@ -1,63 +1,27 @@
-const Plane = require('./Plane');
+const Plane = require("./Plane");
 
-class ExperimentalPlane   extends Plane
-{
+class ExperimentalPlane extends Plane {
+  constructor(planeInformation) {
+    super(
+      planeInformation.model,
+      planeInformation.maxSpeed,
+      planeInformation.maxFlightDistance,
+      planeInformation.maxLoadCapacity
+    );
+    this.type = planeInformation.type;
+    this.classificationLevel = planeInformation.classificationLevel;
+  }
 
-    constructor(planeInformation)  {
-        super(planeInformation.model, planeInformation.maxSpeed, planeInformation.maxFlightDistance, planeInformation.maxLoadCapacity);
-        this.type = planeInformation.type;
-        this.classificationLevel = planeInformation.classificationLevel;
-    }
+
+  get maxLoadCapacity() {
+    return this._maxLoadCapacity;
+  }
+
+  set maxLoadCapacity(value) {
+    this._maxLoadCapacity = value;
+  }
 
 
-    get model()
-    {
-        return this._model;
-    }
-
-    set model(value) {
-        this._model = value;
-    }
-
-    get maxSpeed() {
-        return this._maxSpeed;
-    }
-
-    set maxSpeed(value) {
-        this._maxSpeed = value;
-    }
-
-    get maxFlightDistance() {
-        return this._maxFlightDistance;
-    }
-
-    set maxFlightDistance(value) {
-        this._maxFlightDistance = value;
-    }
-
-    get maxLoadCapacity() {
-        return this._maxLoadCapacity;
-    }
-
-    set maxLoadCapacity(value) {
-        this._maxLoadCapacity = value;
-    }
-
-    get type() {
-        return this._type;
-    }
-
-    set type(value) {
-        this._type = value;
-    }
-
-    get classificationLevel() {
-        return this._classificationLevel;
-    }
-
-    set classificationLevel(value) {
-        this._classificationLevel = value;
-    }
 }
 
-module.exports = ExperimentalPlane
+module.exports = ExperimentalPlane;
