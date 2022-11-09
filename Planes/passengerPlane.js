c65bd295f8366f4d666ec17dd217f489e556f63e
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-11-09 17:20:47 +0300

    Вернул файлы с нормальным наименованием

---
+++ Planes/passengerPlane.js
@@ -0,0 +1,14 @@
+const Plane = require("./Plane");
+
+class PassengerPlane extends Plane {
+  constructor(planeInformation) {
+    super(planeInformation);
+    this.passengersCapacity = planeInformation.passengersCapacity;
+  }
+
+  getPassengersCapacity() {
+    return this.passengersCapacity;
+  }
+}
+
+module.exports = PassengerPlane;
