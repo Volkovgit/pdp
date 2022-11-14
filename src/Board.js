89d99d4c606b22911552c0f429c75d84b270e541
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-11-14 17:42:29 +0300

    Фиксирую рабочую версию бесконечной игры

--- src/Board.js
+++ src/Board.js
@@ -1,36 +1,19 @@
-const BoardPrinter = require('./BoardPrinter');
-
 function Board() {
-  const height = 3;
-  const width = 3;
-  // Найти в чем причина, при обращении по индексу к массиву от сюда заменяется весь столбец
-  const createBoard = () => {
-    const rows = Array(width).fill(' ');
-    const table = Array(height).fill(rows);
-    return table;
-  };
-
-  const boardObject = {
+  return {
     board: [ [ ' ', ' ', ' ' ], [ ' ', ' ', ' ' ], [ ' ', ' ', ' ' ] ],
-    printer: new BoardPrinter(),
     getBoard() {
       return this.board;
     },
-    updateBoard(newBoard) {
-      this.board = newBoard;
-    },
     printBoard(){
-        this.printer.printBoard(this.board);
+        this.board.forEach(row=>{
+          console.log('|',row.join(' | '),'|');
+        })
+
     },
-    setElementToBoard(i,j,elem){
-        this.board[i][j]=elem;
-    },
-    clearBoard(){
-        this.board = [ [ ' ', ' ', ' ' ], [ ' ', ' ', ' ' ], [ ' ', ' ', ' ' ] ];
+    setElementToBoard(coordinates,elem){
+        this.board[coordinates[0]-1][coordinates[1]-1]=elem;
     }
   };
-
-  return boardObject;
 }
 
 module.exports = Board;
--- src/game.js
+++ src/game.js
@@ -1,27 +1,36 @@
-const Board = require("./Board");
-const userInput = require("./userInput");
+const Board = require('./Board');
+const userInput = require('./userInput');
+const inputValidator = require('./inputValidator');
 
-function Game(){
-    const boardObj = Board();
-    const inputObj = userInput()
-    const gameObject = {
-        boardObj,
-        inputObj,
-        playerType:1,
-        startGame(){
-            this.welcome();
-            this.boardObj.printBoard(this.boardObj.getBoard())
-        },
-        welcome(){
-            console.log("Welcome in tic-tac game\nYou wll be X gamer\nGame area:");
-        },
-        iteration(){
-            this.inputObj.input()('Введите строку и столбец :').then(data=>console.log(data))
-        }
-    }
-    return gameObject
+function Game() {
+  const boardObj = Board();
+  const validator = inputValidator();
+  const input = userInput();
+  async function userInputCoordinates(text) {
+    return await input.input(`${text} `).then((data) => {
+      try {
+        return validator.validationInputCoordinates(data);
+      } catch (error) {
+        console.log(error);
+        return userInputCoordinates(text);
+      }
+    });
+  }
+
+  return {
+    boardObj,
+    validator,
+    input,
+    startGame() {
+      this.boardObj.printBoard();
+      this.userInputCoordinates('Введите координаты').then(data=>{
+        this.boardObj.setElementToBoard(data,'x')
+        this.startGame()
+      })
+    },
+    userInputCoordinates
+  };
 }
 
 
-Game().startGame()
-Game().iteration()
+Game().startGame();
