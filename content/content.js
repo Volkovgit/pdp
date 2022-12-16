b4b6c704e79400c3d26a2b89f7ee5efa31ed1074
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-12-16 15:50:18 +0300

    Внес правки по верстке и файлам проекта

--- content/content.js
+++ content/content.js
@@ -1,13 +1,13 @@
-const firstPageContent = document.querySelector(".firstPageContent");
-const secondPageContent = document.querySelector(".secondPageContent");
-const firdPageContent = document.querySelector(".firdPageContent");
-const contents = [
-  document.querySelector(".firstPageContent"),
-  document.querySelector(".secondPageContent"),
-  document.querySelector(".firdPageContent"),
-];
+// const firstPageContent = document.querySelector(".firstPageContent");
+// const secondPageContent = document.querySelector(".secondPageContent");
+// const firdPageContent = document.querySelector(".firdPageContent");
+// const contents = [
+//   document.querySelector(".firstPageContent"),
+//   document.querySelector(".secondPageContent"),
+//   document.querySelector(".firdPageContent"),
+// ];
 
-window.onmessage = function (event) {
-  contents.forEach((pageContent) => pageContent.classList.add("disp-none"));
-  contents[event.data - 1].classList.remove("disp-none");
-};
+// window.onmessage = function (event) {
+//   contents.forEach((pageContent) => pageContent.classList.add("disp-none"));
+//   contents[event.data - 1].classList.remove("disp-none");
+// };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const navRadioButtons = document.querySelectorAll('input[name="nav_radio"]');
-const frameContent = document.querySelector('.content');
+const frameContent = document.querySelector(".content");
 
 navRadioButtons.forEach((radio) =>
   radio.addEventListener("change", function (event) {
@@ -8,7 +8,15 @@ navRadioButtons.forEach((radio) =>
   })
 );
 
+const checkBox = document.querySelector("#openSidebarMenu");
+const menuCheckBox = document.querySelector("#sidebar_container");
 
-
-
-
+checkBox.addEventListener("change", function (event) {
+  if (checkBox.checked) {
+    menuCheckBox.classList.add("sidebar_container_visible");
+    menuCheckBox.classList.remove("sidebar_container_hide");
+  } else {
+    menuCheckBox.classList.add("sidebar_container_hide");
+    menuCheckBox.classList.remove("sidebar_container_visible");
+  }
+});
