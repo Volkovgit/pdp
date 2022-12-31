9c5171fbd97ec26b52f1913baf8abbb5dbd15a47
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-12-31 22:02:00 +0300

    правка замечаний w3org

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import {setCheckBoxesEvent} from './components/header/navigation/slideMenuButton/slideMenuButton.js'
 setCheckBoxesEvent()
 
+
 import { setEventListenerToSidebar } from './components/main-content/sidebar/sidebar.js'
 setEventListenerToSidebar()
