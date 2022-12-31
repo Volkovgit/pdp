b74d412be204a944aa5a7bfe9352fa17d73dcfb6
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-12-31 21:36:13 +0300

    Разбир контент на 3 страницы, прижал футер к низу экрана

--- components/main-content/content/content.js
+++
@@ -1,7 +0,0 @@
-const contents = Array.from(document.querySelectorAll(".content-page"));
-
-window.onmessage = function (event) {
-  console.log(event.data);
-  contents.forEach((pageContent) => pageContent.classList.add("disp-none"));
-  contents[event.data - 1].classList.remove("disp-none");
-};
--- components/main-content/sidebar/sidebar.js
+++ components/main-content/sidebar/sidebar.js
@@ -1,11 +1,15 @@
 export function setEventListenerToSidebar() {
   const navRadioButtons = document.querySelectorAll(".sidebar-button__input");
   const frameContent = document.querySelector(".content-frame");
-
+  const pages={
+    testContent:'./components/main-content/content/testsContent.html',
+    homeContent:'./components/main-content/content/homeTasksContent.html',
+    aboutContent:'./components/main-content/content/aboutContent.html'
+  }
   navRadioButtons.forEach((radio) =>
     radio.addEventListener("change", function (event) {
       let pageNumber = event.target.value;
-      frameContent.contentWindow.postMessage(pageNumber, "*");
+      frameContent.setAttribute('src',pages[pageNumber])
     })
   );
 }
