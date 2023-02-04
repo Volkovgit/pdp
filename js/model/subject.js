e330b3799fcad9631431beb6171c93ce330301e8
Aleksandr Volkov <guthub.volkov@gmail.com>
2023-02-04 23:33:50 +0300

    Init proj

--- js/model/subject.js
+++
@@ -1,7 +0,0 @@
-function Subject(type,answer,number,scorePerSubject){
-  this.type = type;
-  this.answer = answer;
-  this.number = number;
-  this.scorePerSubject = scorePerSubject;
-  this.scores = 0;
-}
